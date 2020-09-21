8deb5bb4383e92709a0d710cb674c416e559cb7f
Germán Téllez Vanegas <gertellezv@gmail.com>
2020-09-21 15:09:56 -0500

    [upd] CORS

--- api-rest/koopers/settings.py
+++ api-rest/koopers/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     #Third party apps
     'widget_tweaks',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'koopers.urls'
 
