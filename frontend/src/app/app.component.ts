8b22734c4ead68354513a8d64590b85ede70f6d1
masmelacarol <carolstefannym@gmail.com>
2020-09-22 17:48:47 -0500

    [frontend/fix]: http request getUser

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './core/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
+  providers: [AuthService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kooper';
 
-  constructor(private authService: AuthService) {
-    this.authService
-      .login('gertellezv@gmail.com', 'platzi2020')
-      .subscribe((data) => console.log(data));
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getUser().subscribe((data) => {
+      console.log('AppComponent -> ngOnInit -> data', data);
+    });
   }
 }
