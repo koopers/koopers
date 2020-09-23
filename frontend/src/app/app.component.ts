bcdc9d71917afd1bc846c5b4b11c7628fa00779c
masmelacarol <carolstefannym@gmail.com>
2020-09-22 20:24:01 -0500

    [frontend/add] button component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faCheckCircle,
+  faEdit,
+  faPlus,
+  faSearch,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from './core/services/auth/auth.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +15,11 @@ import { AuthService } from './core/services/auth/auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'kooper';
+  faSearch = faSearch;
+  faCheckCircle = faCheckCircle;
+  faTrash = faTrash;
+  faPlus = faPlus;
+  faEdit = faEdit;
 
   constructor(private authService: AuthService) {}
 
