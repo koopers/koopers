a86189260752bfc823c6fcc85ca888a02fd4c102
masmelacarol <carolstefannym@gmail.com>
2020-09-23 13:26:08 -0500

    [frontend/upd]: update interfaces and typed functions

--- frontend/src/app/core/services/alerts/alerts.service.ts
+++ frontend/src/app/core/services/alerts/alerts.service.ts
@@ -1,40 +1,45 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { AlertModel, SUCCESS_TYPE, INFO_TYPE, ERROR_TYPE } from 'src/app/shared/components/alerts/alerts.component';
+import { EventEmitter, Injectable } from '@angular/core';
+import {
+  Alert,
+  ERROR_TYPE,
+  INFO_TYPE,
+  SUCCESS_TYPE,
+} from '../../models/alerts';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AlertsService {
-  alerts: AlertModel[] = []
-  showAlert = new EventEmitter<AlertModel[]>();
-  closeAlert = new EventEmitter<AlertModel>();
+  alerts: Alert[] = [];
+  showAlert = new EventEmitter<Alert[]>();
+  closeAlert = new EventEmitter<Alert>();
 
-  constructor() { }
+  constructor() {}
 
-  addAlert(alert: AlertModel) {
-    this.alerts.push(alert)
+  addAlert(alert: Alert): void {
+    this.alerts.push(alert);
   }
 
-  handleAlert(alert: AlertModel) {
-    this.addAlert(alert)
-    this.showAlert.emit(this.alerts)
+  handleAlert(alert: Alert): void {
+    this.addAlert(alert);
+    this.showAlert.emit(this.alerts);
   }
 
-  handleSuccessAlert(message: string) {
-    this.handleAlert({message, type: SUCCESS_TYPE})
+  handleSuccessAlert(message: string): void {
+    this.handleAlert({ message, type: SUCCESS_TYPE });
   }
 
-  handleInfoAlert(message: string) {
-    this.handleAlert({message, type: INFO_TYPE})
+  handleInfoAlert(message: string): void {
+    this.handleAlert({ message, type: INFO_TYPE });
   }
 
-  handleErrorAlert(message: string) {
-    this.handleAlert({message, type: ERROR_TYPE})
+  handleErrorAlert(message: string): void {
+    this.handleAlert({ message, type: ERROR_TYPE });
   }
 
-  onAlertClosed(alert: AlertModel) {
+  onAlertClosed(alert: Alert): void {
     const index = this.alerts.indexOf(alert);
-    if(index !== -1) {
+    if (index !== -1) {
       this.alerts.splice(index, 1);
     }
     this.closeAlert.emit(alert);
--- frontend/src/app/core/services/auth/auth.service.ts
+++ frontend/src/app/core/services/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,44 +8,7 @@ import { environment } from '../../../../environments/environment';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  getCookie(cname) {
-    const name = cname + '=';
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    let c;
-    for (let i = 0; i < ca.length; i++) {
-      c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return c;
-  }
-
-  login(email, password) {
-    const cookie = this.getCookie('csrftoken');
-    console.log('AuthService -> login -> cookie', cookie);
-    return this.http.post(
-      `${environment.url_api}/auth/signin/`,
-      {
-        username: email,
-        password,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          mode: 'cors',
-          'Access-Control-Allow-Origin': '*',
-          'X-CSRFToken': cookie,
-        },
-      }
-    );
-  }
-
-  getUser(): Observable<any> {
+  getUser(): Observable<{}> {
     return this.http.get('/auth/user/');
   }
 }
