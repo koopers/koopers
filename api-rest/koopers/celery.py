b9d71c9c456ec38cf8583d089a1a30dac96784d2
Germán Téllez <gertellezv@gmail.com>
2020-09-24 01:31:01 -0500

    [backend/add] Screenshot Periodic task

--- api-rest/koopers/celery.py
+++ api-rest/koopers/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koopers.settings')
@@ -20,14 +21,16 @@ app.autodiscover_tasks()
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
-from celery.schedules import crontab
-
 app.conf.beat_schedule = {
     "add-every-minute-contrab": {
         "task": "sum_two_numbers",
-        "schedule": crontab(),
-        # "args": (16, 20),
+        "schedule": crontab()
     },
+    # "add-every-minute-contrab": {
+    #     "task": "sum_two_numbers",
+    #     "schedule": crontab(),
+    #     # "args": (16, 20),
+    # },
     # "add-every-5-seconds": {
     #     "task": "multiply_two_numbers",
     #     "schedule": 5.0,  # in seconds
