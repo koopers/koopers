94e9d7574927326e45f083fb8c1a494b5858d72d
Josh Wolf <alienxtrix@gmail.com>
2020-09-25 12:09:55 -0500

    header component

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AlertsComponent } from './components/alerts/alerts.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AlertsComponent],
+  declarations: [
+    AlertsComponent,
+    HeaderComponent],
   imports: [
     CommonModule,
     FontAwesomeModule
   ],
   exports: [
-    AlertsComponent
+    AlertsComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule { }
