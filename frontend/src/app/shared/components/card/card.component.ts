ce055ae9e134025a1304214d6be7cde00c7b5f3d
zomars <zomars@me.com>
2020-09-28 10:31:53 -0600

    [frontend/upd} Updates card component template

--- frontend/src/app/shared/components/card/card.component.ts
+++ frontend/src/app/shared/components/card/card.component.ts
@@ -1,19 +1,24 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Site } from 'src/app/core/models/sites';
 
+interface CardItem {
+  title: string;
+  description?: string;
+  date?: string;
+  href: string;
+  image_src_mobile: string;
+  image_src_tablet: string;
+  image_src_desktop: string;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.sass'],
 })
 export class CardComponent implements OnInit {
-  @Input() item: Site;
-  @Input() href: string;
-  @Input() title: string;
-  @Input() imageSrc: string;
-  @Input() description: string;
-  @Input() date: string;
-  @Output() clickCard: EventEmitter<string> = new EventEmitter();
+  @Input() item: CardItem;
+  @Output() clickCard?: EventEmitter<string> = new EventEmitter();
 
   constructor() {}
 
