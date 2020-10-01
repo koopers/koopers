13db8a7dd2d9ce47a5d176767aafb96de6a8749a
masmelacarol <carolstefannym@gmail.com>
2020-09-30 23:48:33 -0500

    [frontend/upd][refs #57]: page detail sites

--- frontend/src/app/sites/sites.module.ts
+++ frontend/src/app/sites/sites.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '@shared/shared.module';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
 import { DetailsComponent } from './components/details/details.component';
 import { SearchPageComponent } from './components/search-page/search-page.component';
 import { SitesRoutingModule } from './sites-routing.module';
@@ -12,7 +15,10 @@ import { SitesRoutingModule } from './sites-routing.module';
   imports: [
     CommonModule,
     SitesRoutingModule,
-    SharedModule
+    SharedModule,
+    CalendarModule,
+    ReactiveFormsModule,
+    DropdownModule,
   ]
 })
 export class SitesModule { }
