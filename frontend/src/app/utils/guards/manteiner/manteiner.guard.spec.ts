3ad8764fb0a87e01f2035f30d5459c6d01ba2669
oscar <oscar.pay4@gmail.com>
2020-10-03 19:52:27 -0500

    [FRONT] fix all tests

--- frontend/src/app/utils/guards/manteiner/manteiner.guard.spec.ts
+++ frontend/src/app/utils/guards/manteiner/manteiner.guard.spec.ts
@@ -4,23 +4,37 @@ import { ManteinerGuard } from './manteiner.guard';
 import { AuthService } from '@core/services/auth/auth.service';
 import { AuthServiceStub } from '@utils/stubs/stubs';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('ManteinerGuard', () => {
   let guard: ManteinerGuard;
+  let authService: AuthService;
+  let routeMock: any = { snapshot: {}};
+  let routeStateMock: any = { snapshot: {}, url: '/cookies'};
+  let routerMock = {navigate: jasmine.createSpy('navigate')}
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        {provide: AuthService, useClass: AuthServiceStub}
+        {provide: AuthService, useClass: AuthServiceStub},
+        { provide: Router, useValue: routerMock }
       ],
       imports: [
         RouterTestingModule
       ]
     });
     guard = TestBed.inject(ManteinerGuard);
+    authService = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should redirect an unauthenticated user to the login route', () => {
+    spyOn(authService, 'getUser').and.returnValue(of({admin: false}));
+    guard.canActivate(routeMock, routeStateMock);
+    expect(routerMock.navigate).not.toHaveBeenCalledWith(['/admin/sites']);
   });
 });
--- frontend/src/app/utils/interceptors/auth/auth.interceptor.spec.ts
+++ frontend/src/app/utils/interceptors/auth/auth.interceptor.spec.ts
@@ -1,23 +1,82 @@
 import { TestBed } from '@angular/core/testing';
-
+import { environment } from '../../../../environments/environment';
 import { AuthInterceptor } from './auth.interceptor';
 import { AuthService } from '@core/services/auth/auth.service';
-import { AuthServiceStub } from '@utils/stubs/stubs';
+import { AuthServiceStub, TokenServiceStub } from '@utils/stubs/stubs';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { CategoriesService } from '@core/services/categories/categories.service';
+import { TokenService } from '@core/services/token/token.service';
 
 describe('AuthInterceptor', () => {
+  let service: CategoriesService;
+  let authService: AuthService;
+  let tokenService: TokenService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
       AuthInterceptor,
-      {provide: AuthService, useClass: AuthServiceStub}
+      AuthService,
+      CategoriesService,
+      {provide: TokenService, useClass: TokenServiceStub},
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptor,
+        multi: true,
+      },
     ],
     imports: [
+      HttpClientTestingModule,
       RouterTestingModule
     ]
   }));
+
+  beforeEach(() => {
+    service = TestBed.inject(CategoriesService);
+    authService = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
+    tokenService = TestBed.inject(TokenService);
+  });
 
   it('should be created', () => {
     const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
     expect(interceptor).toBeTruthy();
   });
+
+  it('should not add an Authorization header', () => {
+    service.getAll().subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+    const httpRequest = httpMock.expectOne(`${environment.url_api}/categories/`);
+    expect(httpRequest.request.headers.has('Authorization')).toEqual(false);
+  });
+
+  it('should not add an Authorization header (refresh)', () => {
+    authService.refreshToken().subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+    const httpRequest = httpMock.expectOne(`${environment.url_api}/auth/signin/refresh/`);
+    expect(httpRequest.request.headers.has('Authorization')).toEqual(false);
+  });
+
+  it('should add an Authorization header', () => {
+    spyOn(tokenService, 'getToken').and.returnValue('TOKEN');
+    service.getAll().subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+    const httpRequest = httpMock.expectOne(`${environment.url_api}/categories/`);
+    expect(httpRequest.request.headers.has('Authorization')).toEqual(true);
+  });
+
+  it('should not add an Authorization header (refresh)', () => {
+    spyOn(tokenService, 'getRefreshToken').and.returnValue('TOKEN');
+    authService.refreshToken().subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+    const httpRequest = httpMock.expectOne(`${environment.url_api}/auth/signin/refresh/`);
+    expect(httpRequest.request.headers.has('Authorization')).toEqual(true);
+  });
+
 });
